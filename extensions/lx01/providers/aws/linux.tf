d1488357005fb35d909eb57bb84aa77c886d66bb
David Álvarez Robles <58595089+tdkmp4n4@users.noreply.github.com>
2025-10-30 00:40:46 +0100

    Update linux.tf

--- extensions/lx01/providers/aws/linux.tf
+++ extensions/lx01/providers/aws/linux.tf
@@ -2,8 +2,9 @@
   name               = "lx01"
   linux_sku          = "22_04-lts-gen2"
   linux_version      = "latest"
-  ami                = "ami-00c71bd4d220aa22a"
+  ami                = "ami-0ef0fafba270833fc"
   private_ip_address = "{{ip_range}}.32"
   password           = "HGLXP@ssw_rd$"
   size               = "t2.micro"  # 1cpu / 1GB
+  instance_type      = "t2.medium"  # 2cpu / 4GB
 }
