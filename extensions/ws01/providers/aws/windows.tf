c12dcd506fff7555ab5508dba4c70c5328d4c84e
David Álvarez Robles <58595089+tdkmp4n4@users.noreply.github.com>
2025-10-30 00:41:32 +0100

    Update windows.tf

--- extensions/ws01/providers/aws/windows.tf
+++ extensions/ws01/providers/aws/windows.tf
@@ -3,7 +3,7 @@
   name               = "ws01"
   domain             = "sevenkingdoms.local"
   windows_sku        = "2019-Datacenter"
-  ami                = "ami-03440f0d88fea1060"
+  ami                = "ami-0396eefd70fa23639"
   instance_type      = "t2.medium"
   private_ip_address = "{{ip_range}}.31"
   password           = "EP+xh7Rk6j90"
