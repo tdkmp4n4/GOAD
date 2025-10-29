fef0beee2b2437f1d98826b4d5b6da1b0809288b
David Álvarez Robles <58595089+tdkmp4n4@users.noreply.github.com>
2025-10-30 00:39:11 +0100

    Update linux.tf

--- extensions/elk/providers/aws/linux.tf
+++ extensions/elk/providers/aws/linux.tf
@@ -2,8 +2,9 @@
   name               = "elk"
   linux_sku          = "22_04-lts-gen2"
   linux_version      = "latest"
-  ami                = "ami-00c71bd4d220aa22a"
+  ami                = "ami-0ef0fafba270833fc"
   private_ip_address = "{{ip_range}}.50"
   password           = "654qsdIazajsQ*"
   size               = "t2.medium"  # 2cpu / 4GB
+  instance_type      = "t2.medium"  # 2cpu / 4GB
 }
