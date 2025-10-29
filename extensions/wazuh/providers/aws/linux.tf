d43ed44ce8d3db851a5e3f86b64ab2dfe810790b
David Álvarez Robles <58595089+tdkmp4n4@users.noreply.github.com>
2025-10-30 00:41:12 +0100

    Update linux.tf

--- extensions/wazuh/providers/aws/linux.tf
+++ extensions/wazuh/providers/aws/linux.tf
@@ -2,8 +2,9 @@
   name               = "wazuh"
   linux_sku          = "22_04-lts-gen2"
   linux_version      = "latest"
-  ami                = "ami-00c71bd4d220aa22a"
+  ami                = "ami-0ef0fafba270833fc"
   private_ip_address = "{{ip_range}}.51"
   password           = "sgdvnkjhdshlsd"
   size               = "t2.large"  # 2cpu / 8GB
+  instance_type      = "t2.large"  # 2cpu / 8GB
 }
